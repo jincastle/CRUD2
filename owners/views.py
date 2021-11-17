6dd41f6480205184985c47a43e9deab5bf037c3d
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-11-17 15:04:49 +0900

    CURD#2

--- owners/views.py
+++ owners/views.py
@@ -3,23 +3,12 @@ from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.views import View
 from .models import Owner, Dog
+#json file -> python file
 import json
 
 # Create your views here.
 
 class OwnersView(View):
-    # def post(self, request):
-    #     data = json.loads(request.body)
-    #     owner = Owner.objects.create(
-    #         name=data["owner"],
-    #         )
-    #     dog = Dog.objects.create(
-    #         name=data["dog"], 
-    #         owner=owner,
-    #         age = data["age"]
-    #         )
-
-    #     return JsonResponse({"result" : CREATE}, status = 201)
     def post(self, request):
         data = json.loads(request.body)
         owner = Owner.objects.create(
@@ -27,22 +16,23 @@ class OwnersView(View):
             email=data["email"],
             age=data["age"]
         )
-        return JsonResponse({"result" : "CREATE"}, status = 201)
+        return JsonResponse({"result" : "CREATE"}, status=201)
+
 
     def get(self, request):
-        owner_list = Owner.objects.all()
-        a = Dog.objects.all()
-        results = []
-        for owner in owner_list:
-            results.append(
+        owner = Owner.objects.all()
+        requests = []
+        for owners in owner:
+            requests.append(
                 {
-                    "name" : owner.name,
-                    "email" : owner.email,
-                    "age" : owner.age,
-                    "dog_name" : list(owner.dog_set.values('name'))
+                    "name" : owners.name,
+                    "email" : owners.email,
+                    "age" : owners.age,
+                    "dog_name" : list(owners.dog_set.values('name'))       
                 }
             )
-        return JsonResponse({"result" : results}, status = 201)
+
+        return JsonResponse({"result" : requests}, status = 201)
 
         
 
@@ -62,13 +52,43 @@ class DogsView(View):
         results = []
         for dog in dogs:
             results.append(
-                {
+                {   
+                    "id" : dog.id,
                     "name" : dog.name,
                     "age" : dog.age,
-                    "owner" : dog.owner.name,
+                    "owner" : dog.owner.name
                 }
             )
         return JsonResponse({"result" : results}, status = 201)
 
+# 정참조 없이 구현 
+# for dog in dogs:
+#     owner = Owner.objects.get(dog__id=dog.id)
+#     results.append({
+#         "id" : dog.id,
+#         "name" : dog.name,
+#         "age" : dog.age,
+#         "owner" : {
+#             "id" : owner.id,
+#             "name" : owner.name,
+#         }
+#     })
 
 
+# for owner in owners:
+#     dogs = Dog.objects.filter(owner_id = owner.id)
+#     dog_list = []
+
+
+# class MoviesView(View):
+#     def get(self,request):
+#         ac = Actor.objects.all()
+#         results=[]
+#         for a in ac:
+#             results.append(
+#                 {
+#                     "first_name" : a.first_name,
+#                     "movies" : [i.title for i in a.movies.all()]                    
+#                 }
+#             )
+#         return JsonResponse({"result":results}, status = 201)
